a5b52a7852ec5a510add00380fac298a3acd66d5
Artie Fischer <artie@curetechnologies.us>
2021-04-02 15:08:12 -0600

    fix: removes console.log

--- src/_services/messages.service.js
+++ src/_services/messages.service.js
@@ -4,7 +4,6 @@ const msgSubject = new Subject();
 
 export const messagesService = {
     sendMessage: message => {
-        console.log("CLICKED:", message)
         return msgSubject.next({ text: message })
     },
     clearMessages: () => msgSubject.next(),
