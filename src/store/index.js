d83ce6a057d46bb3d4532bc3a2c22c596279b9b7
Artie Fischer <artie@curetechnologies.us>
2021-04-02 21:29:05 -0600

    fix: simplifies store action to dismiss toasts

--- src/store/index.js
+++ src/store/index.js
@@ -13,25 +13,15 @@ export default createStore({
     getToasts (state) {
       return state.toasts
     },
-    resetToasts (state) {
-      state.toasts = []
-    },
-    resetToast (state) {
+    clearToast (state) {
       state.toasts.pop()
     },
   },
   actions: {
-    resetAsync({commit, state}) {
-      if (state.toasts.length > 0) {
+    resetAsync({commit}) {
         setTimeout(() => {
-          commit("resetToast")
+          commit("clearToast")
         }, 1700)
-      } else {
-        setTimeout(() => {
-          commit("resetToasts")
-        }, 1700)
-      }
-
     }
   },
   modules: {
