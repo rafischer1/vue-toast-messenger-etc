8c64ef403ee03f033a85a6e7ee33eb5fadde17dd
Artie Fischer <artie@curetechnologies.us>
2021-04-02 21:51:13 -0600

    feat: trash button removes single message

--- src/components/messages/messages.vue
+++ src/components/messages/messages.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="messages">
-    <div v-for="(message, index) in messages" :key="index">{{message.text}}</div>
+    <div v-for="(message, index) in messages" :key="index">{{message.text}}
+      <span class="trash" @click="deleteMsg(index)">x</span>
+    </div>
   </div>
 </template>
 
@@ -17,7 +19,6 @@ name: "messages", data () {
     this.subscription = messagesService.getMessage().subscribe(message => {
       if (message) {
         // add message to local state if not empty
-        console.log("message here:", message)
         this.messages.push(message);
       } else {
         // clear messages when empty message received
@@ -28,6 +29,12 @@ name: "messages", data () {
   beforeUnmount () {
     // unsubscribe to ensure no memory leaks
     this.subscription.unsubscribe();
+  },
+  methods: {
+   deleteMsg(index) {
+     this.messages.splice(index, 1)
+     this.$store.commit("showToast", {text: "Deleted", type: "warn"})
+   },
   }
 };
 </script>
@@ -39,9 +46,26 @@ name: "messages", data () {
   }
 
   .messages > div {
+    border-radius: 5px;
     text-align: center;
     margin-top: 5px;
     background: whitesmoke;
     color: #333333;
+  }
+
+  .trash {
+    float: right;
+    margin: -3px 5px 3px 0;
+    background: none;
+    cursor: pointer;
+    color: #333333;
+  }
+
+  .trash:hover {
+    color: #db4a4a;
+  }
+
+  .trash:active {
+    color: #ff7171;
   }
 </style>
